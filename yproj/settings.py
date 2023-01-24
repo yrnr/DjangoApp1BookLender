bb56509a376c0d9711737f73035c70e94259f179
yrnr <60417312+yrnr@users.noreply.github.com>
2023-01-25 04:43:27 +0530

    Update settings.py
    Invalidated the secret key

--- yproj/settings.py
+++ yproj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6&z@nf#ro3tgn!*#0w+9dxj+k!(eu10pyhn8zi!*$6--sc*&tg'
+SECRET_KEY = 'invalid-django-insecure-6&z@nf#ro3tgn!*#0w+9dxj+k!(eu10pyhn8zi!*$6--sc*&tg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
